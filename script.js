c3b3537e2282797e5be48bb57cbe2d2a07ca8f60
unknown <fullertonlogan@gmail.com>
2023-10-23 13:07:43 -0700

    updated javascript

--- script.js
+++ script.js
@@ -7,3 +7,73 @@
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
+
+const app={
+    init:()=>{
+        document.getElementById('search-btn').addEventListener('click',()=>{
+            let city= document.getElementById('userInput').value;
+            if(!city){
+                return 
+            }
+            app.fetchCoords(city)
+        });
+    },
+    
+fetchWeather:(lat,lon)=>{
+    let key = '3f1f6cf4e4d47e3b57f26304e7165e73'
+    let units = 'imperial'
+    let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=${units}`;
+    fetch(url)
+    .then(resp=>{
+        if(!resp.ok) throw new Error(response.statusText)
+        return resp.json();
+    })
+    .then(data=>{
+        //app.showWeather(data);
+        console.log(data)
+        app.renderWeather(data)
+    })
+    .catch(console.err);
+    
+    },
+    fetchCurrentWeather:(lat,lon)=>{
+        let key = '3f1f6cf4e4d47e3b57f26304e7165e73'
+        let units = 'imperial'
+        let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=${units}`;
+        fetch(url)
+        .then(resp=>{
+            if(!resp.ok) throw new Error(response.statusText)
+            return resp.json();
+        })
+        .then(data=>{
+            //app.showWeather(data);
+            console.log(data)
+        })
+        .catch(console.err);
+        
+        },
+    fetchCoords:(city)=>{
+        let key = '3f1f6cf4e4d47e3b57f26304e7165e73'
+        let url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${key}`
+        fetch (url)
+        .then((resp)=>{
+            return resp.json();
+        })
+        .then((data)=>{
+            let {lat,lon}=data[0]
+            app.fetchWeather(lat,lon)
+            app.fetchCurrentWeather(lat,lon)
+        })
+    },
+    renderWeather(data){
+        for(i=0;i<data.list.length;i+=8){
+        let card=document.createElement('div')
+        let tempEl=document.createElement('p')
+        tempEl.textContent=data.list[i].main.tempEl
+        card.append(tempEl)
+        document.getElementById('forecastBox').append(card)
+        }
+
+    }
+}
+app.init()
